718eb53ecc785bc32bca976573a3e77c39365bc4
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-06-17 00:59:56 -0600

    chamghe padding for header

--- wonder-gym/src/components/Header/index.js
+++ wonder-gym/src/components/Header/index.js
@@ -14,7 +14,7 @@ export default function Headers(props){
 
     return (    
     <div className={`flex h-24 ${background} w-full sm:px-8 md:px-12 lg:px-24`}>
-        <div className="flex items-center w-6/12 pl-4">
+        <div className="flex items-center w-6/12 pl-8">
             <img src={logo} alt="logo" />
         </div>
         <div className="flex pr-4 items-center justify-end w-6/12">
