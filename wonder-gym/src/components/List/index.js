f92926dd02af61ddf873b7931e8ec96211ed4356
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-06-17 01:00:22 -0600

    add icons to the list

--- wonder-gym/src/components/List/index.js
+++ wonder-gym/src/components/List/index.js
@@ -15,13 +15,19 @@ export default function Lists(props){
                 
                 return <div class="flex-col mx-auto w-full items-center justify-center bg-main-white">
                         <Link class="font-bold dark:text-white items-center" to={input.route}>
-                            <div class="hover:bg-gray-50 flex flex-1 items-center p-8">
-                                <div class="flex flex-col w-10 h-10 justify-center items-center mr-4"></div>
-                                    <li class="flex flex-row">{input.tag}</li>
+                            <div class="hover:bg-gray-50 flex flex-1 items-center pl-3 pb-8 pt-8">
+                                <div class="flex flex-col w-0 h-10 justify-center items-center mr-4"></div>
+                                <li class="flex flex-row h-1 justify-center items-center mr-4">
+                                    <div class ="pr-6">
+                                        {input.icon}
+                                    </div>
+                                    <div pl-10>
+                                        {input.tag}
+                                    </div>
+                                </li>
                             </div>
                         </Link>
                     </div>
-
             })}
         </>
     );
