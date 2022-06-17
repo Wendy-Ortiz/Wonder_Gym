552fd8faa767ae16025fd612636cfb16490c0cec
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-06-17 01:01:40 -0600

    add icons to the trainer view

--- wonder-gym/src/Pages/Trainer/index.js
+++ wonder-gym/src/Pages/Trainer/index.js
@@ -2,26 +2,25 @@ import React from 'react';
 import Header from '../../components/Header'
 import List from '../../components/List'
 
-const ListItem = (props) => {
+export default function MainTrainer() {
+    const ListItem = (props) => {
     return <p className='m-4'><a href={props.route}> {props.tag} </a></p>;
 };
-export default function MainTrainer() {
-
     const MenuValues = [
     {
         id: 1,
         tag: 'Mi perfil',
-        route: '/trainerProfile'
+        route: '/mainTrainer/trainerProfile'
     },
     {
         id: 2,
         tag: 'Configuraciones',
-        route: '/settings'
+        route: '/mainTrainer/settings'
     },
     {
         id: 3,
         tag: 'Cerrar Sesión',
-        route: '/logOut'
+        route: '/mainTrainer/logOut'
     },
     ]
 
@@ -29,22 +28,31 @@ export default function MainTrainer() {
     {
         id: 1,
         tag: 'Usuarios',
-        route: '/trainerUsers'
+        route: '/mainTrainer/trainerUsers',
+        icon: <svg class="h-8 w-8 text-white"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
+      </svg>
     },
     {
         id: 2,
         tag: 'Nueva Rutina',
-        route: '/newRoutine'
+        route: '/mainTrainer/newRoutine',
+        icon: <svg class="h-8 w-8 text-white"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/>
+      </svg>
+      
     },
     {
         id: 3,
         tag: 'Entrenar',
-        route: '/toTrain'
+        route: '/mainTrainer/toTrain',
+        icon: <svg class="h-8 w-8 text-white"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="5" cy="18" r="3" />  <circle cx="19" cy="18" r="3" />  <polyline points="12 19 12 15 9 12 14 8 16 11 19 11" />  <circle cx="17" cy="5" r="1" /></svg>
     },
     {
         id: 4,
         tag: 'Noticias',
-        route: '/News'
+        route: '/mainTrainer/news',
+        icon: <svg class="h-8 w-8 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />  <polyline points="14 2 14 8 20 8" />  <line x1="16" y1="13" x2="8" y2="13" />  <line x1="16" y1="17" x2="8" y2="17" />  <polyline points="10 9 9 9 8 9" /></svg>
     },
     ]
 
