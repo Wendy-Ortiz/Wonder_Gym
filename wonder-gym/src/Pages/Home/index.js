3ccf200d4c92b3ead7dd9f290e94bffd4559bf8d
AxelMatus <axelantonio@gmail.com>
2022-06-27 21:17:21 -0600

    Created new Login Modal

--- wonder-gym/src/Pages/Home/index.js
+++ wonder-gym/src/Pages/Home/index.js
@@ -1,29 +1,33 @@
+import { useState } from 'react';
 import Header from '../../components/Header'
+import LoginModal from '../../components/LoginModal';
 
 export default function Home() {
+    const [isOpenLoginModal, setIsOpenLoginModal] = useState(false)
 
-  const MenuValues = [
-    {
-        id: 1,
-        tag: 'Iniciar Sesión',
-        init: 'true'
-        //route: '/login',
-    },
-    {
-      id: 2,
-      tag: 'Registrarme',
-      route: '/register'
-    },
-]
+    const MenuValues = [
+      {
+          id: 1,
+          tag: 'Iniciar Sesión',
+          handleClick: () => setIsOpenLoginModal(true),
+      },
+      {
+        id: 2,
+        tag: 'Registrarme',
+        handleClick: () => console.log('Registrarme'),
+      },
+    ]
 
     return (
-      <div className='divide-y-2 divide-main-gold'>
-        <div className='m-0'>
-          <Header bg_gray={true} logo_src='/logo84-64.png' values={MenuValues}/>
+      <>
+        <div className='divide-y-2 divide-main-gold'>
+          <div className='m-0'>
+            <Header bg_gray={true} logo_src='/logo84-64.png' values={MenuValues}/>
+          </div>
+          <div className={`m-0 flex h-screen bg-main-gray w-full sm:px-8 md:px-12 lg:px-24`}>
+          </div>
         </div>
-        <div className={`m-0 flex h-screen bg-main-gray w-full sm:px-8 md:px-12 lg:px-24`}>
-        </div>
-      </div>
-      
+        <LoginModal isOpenLoginModal={isOpenLoginModal} setIsOpenLoginModal={setIsOpenLoginModal} />
+      </>
     );
   }
