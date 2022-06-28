24772c8fa3a74bc0b40da4a6a8a4558731f2be83
Axel Antonio Matus Vargas <b74597@ecci.ucr.ac.cr>
2022-06-28 16:57:31 -0600

    added main img to home

--- wonder-gym/src/Pages/Home/index.js
+++ wonder-gym/src/Pages/Home/index.js
@@ -27,19 +27,29 @@ export default function Home() {
           <div className='m-0'>
             <Header bg_gray={true} logo_src='/logo84-64.png' values={MenuValues}/>
           </div>
-          <div className={`m-0 flex h-screen bg-main-gray w-full px-8 md:px-12 lg:px-24`}>
-            <div className={`font-bebasNeue w-full pt-10 h-20`}>
-              <div className={`flex`}>
-                {/* Decorator*/}
-                <div className={`w-1/12`}><img className={`object-left`} src={`/Home_header_decorator.png`} alt="login logo" /></div>
-                {/* Sub Title*/}
-                <div className={`w-11/12 text-white text-2xl justify-center`}> Prepárate para </div>
+          {/* Main Title*/}
+          <div className={`m-0 flex bg-main-gray h-auto w-full px-8 md:px-12 lg:px-24`}>
+            <div className=''>
+              <div className={`font-bebasNeue w-full pt-10 h-auto`}>
+                <div className={`flex`}>
+                  {/* Decorator*/}
+                  <div className={`w-1/12`}><img className={`object-left`} src={`/Home_header_decorator.png`} alt="login logo" /></div>
+                  {/* Sub Title*/}
+                  <div className={`w-11/12 text-white text-2xl justify-center`}> Prepárate para </div>
+                </div>
+                <div className={`pl-[8.333333%] text-5xl`}>
+                  <p className={`text-white`}> MOTIVARTE, SUPERARTE Y CONVERTIRTE EN</p>
+                  <p className={`text-main-gold`}> TU MEJOR VERSIÓN </p>
+                </div>
               </div>
-              <div className={`pl-[8.333333%] text-5xl`}>
-                <p className={`text-white`}> MOTIVARTE, SUPERARTE Y CONVERTIRTE EN</p>
-                <p className={`text-main-gold`}> TU MEJOR VERSIÓN </p>
+              <div  className='-mx-8 md:-mx-12 lg:-mx-24 mt-8'>
+                <img className='object-cover' src={'/foto_gym_01.png'} alt="logo" />
               </div>
             </div>
+          </div>
+          {/* Another Section*/}
+          <div className='bg-main-gray h-screen'>
+
           </div>
         </div>
         <LoginModal isOpenLoginModal={isOpenLoginModal} setIsOpenLoginModal={setIsOpenLoginModal} />
