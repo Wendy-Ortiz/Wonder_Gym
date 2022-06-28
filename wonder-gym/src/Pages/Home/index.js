fbdc48b6f0100da9ddd39f4ed2d0f8349c3d398f
AxelMatus <axelantonio@gmail.com>
2022-06-28 03:58:59 -0600

    Added main title to Home Page

--- wonder-gym/src/Pages/Home/index.js
+++ wonder-gym/src/Pages/Home/index.js
@@ -1,9 +1,12 @@
 import { useState } from 'react';
+import { useNavigate  } from 'react-router-dom'
+
 import Header from '../../components/Header'
 import LoginModal from '../../components/LoginModal';
 
 export default function Home() {
     const [isOpenLoginModal, setIsOpenLoginModal] = useState(false)
+    const navigate = useNavigate ();
 
     const MenuValues = [
       {
@@ -14,7 +17,7 @@ export default function Home() {
       {
         id: 2,
         tag: 'Registrarme',
-        handleClick: () => console.log('Registrarme'),
+        handleClick: () => navigate(`/register`),
       },
     ]
 
@@ -24,7 +27,19 @@ export default function Home() {
           <div className='m-0'>
             <Header bg_gray={true} logo_src='/logo84-64.png' values={MenuValues}/>
           </div>
-          <div className={`m-0 flex h-screen bg-main-gray w-full sm:px-8 md:px-12 lg:px-24`}>
+          <div className={`m-0 flex h-screen bg-main-gray w-full px-8 md:px-12 lg:px-24`}>
+            <div className={`font-bebasNeue w-full pt-10 h-20`}>
+              <div className={`flex`}>
+                {/* Decorator*/}
+                <div className={`w-1/12`}><img className={`object-left`} src={`/Home_header_decorator.png`} alt="login logo" /></div>
+                {/* Sub Title*/}
+                <div className={`w-11/12 text-white text-2xl justify-center`}> Prepárate para </div>
+              </div>
+              <div className={`pl-[8.333333%] text-5xl`}>
+                <p className={`text-white`}> MOTIVARTE, SUPERARTE Y CONVERTIRTE EN</p>
+                <p className={`text-main-gold`}> TU MEJOR VERSIÓN </p>
+              </div>
+            </div>
           </div>
         </div>
         <LoginModal isOpenLoginModal={isOpenLoginModal} setIsOpenLoginModal={setIsOpenLoginModal} />
