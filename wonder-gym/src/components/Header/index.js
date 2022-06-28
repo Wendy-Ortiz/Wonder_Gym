dced7e58eb18273637e725b450f613e75a372bc5
AxelMatus <axelantonio@gmail.com>
2022-06-27 21:16:31 -0600

    Header restructured

--- wonder-gym/src/components/Header/index.js
+++ wonder-gym/src/components/Header/index.js
@@ -24,17 +24,20 @@ const useWindowDimensions = () => {
     return windowDimensions;
 }
 
-const createMenuButtons = (input, handleClick) => {
+const createMenuButtons = (input, setShowUserMenu, handleClick) => {
+    const handleButtonClick = () => {
+        setShowUserMenu(false);
+        handleClick()
+    }
+
     return (
-        <>
-        <button className={`flex p-0 w-full h-20 text-black justify-center items-center`}key={input.id} onClick={handleClick} >
+        <button className={`flex p-0 w-full h-20 text-black justify-center items-center`} key={input.id} onClick={handleButtonClick} >
             {input.tag}
         </button>
-        </>
     );
 }
 
-const createMenu = (values, handleClick, showUserMenu, setShowUserMenu, width) => {
+const createMenu = (values, showUserMenu, setShowUserMenu, width) => {
     const commonProps = `bg-stone-300 absolute top-[6.15rem] h-auto w-[200px] divide-y divide-stone-500`
     let menuPosition = width <= 1024
         ? `${commonProps} left-0 ${showUserMenu ? 'translate-x-0':'-translate-x-full'} ease-in-out duration-300`
@@ -45,7 +48,7 @@ const createMenu = (values, handleClick, showUserMenu, setShowUserMenu, width) =
         <div onClick={() => { setShowUserMenu(false); }} className="absolute top-[6.25rem] left-0 h-full w-full"></div>
             <div className={menuPosition}>
             {values.map((input) => (
-                createMenuButtons( input, handleClick)
+                createMenuButtons(input, setShowUserMenu, input.handleClick)
             ))}
         </div>
         </>
@@ -60,14 +63,12 @@ export default function Headers(props){
 
     const [showUserMenu, setShowUserMenu] = useState(false);
 
-    const handleClick = () => {console.log('click')};
-
     return (
     width <= 1024 ?
     <div className={`flex h-24 ${background} w-full sm:px-8 md:px-12 lg:px-24`}>
         <div className="flex items-center w-6/12 pl-4 text-white">
-            <FiMenu color={icon_color} size="2rem" onClick={() => {  showUserMenu ? setShowUserMenu(false) : setShowUserMenu(true); }} className="cursor-pointer"/>
-            {createMenu(props.values, handleClick, showUserMenu, setShowUserMenu, width)}
+            <FiMenu color={icon_color} size="2rem" onClick={() => { showUserMenu ? setShowUserMenu(false) : setShowUserMenu(true); }} className="cursor-pointer"/>
+            {createMenu(props.values, showUserMenu, setShowUserMenu, width)}
         </div>
         <div className="flex pr-4 items-center justify-end w-6/12">
             <img src={logo} alt="logo" />
@@ -80,7 +81,7 @@ export default function Headers(props){
         </div>
         <div className="flex pr-4 items-center justify-end w-6/12">
             <FiMenu color={icon_color} size="2rem" onClick={() => { showUserMenu ? setShowUserMenu(false) : setShowUserMenu(true); }} className="cursor-pointer"/>
-            {createMenu(props.values, handleClick, showUserMenu, setShowUserMenu, width) }
+            {createMenu(props.values, showUserMenu, setShowUserMenu, width)}
         </div>
     </div>
     );
