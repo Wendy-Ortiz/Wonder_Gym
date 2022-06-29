92af217fa5a0ed24e9f8ace23a87b1ef4e24e091
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-06-29 10:13:10 -0600

    add flag for search bar

--- wonder-gym/src/Pages/Trainer/TrainerUsers.js
+++ wonder-gym/src/Pages/Trainer/TrainerUsers.js
@@ -4,9 +4,8 @@ import List from '../../components/List'
 import SecondHeader from '../../components/SecondHeader'
 
 export default function MainTrainer() {
-    const ListItem = (props) => {
-    return <p className='m-4'><a href={props.route}> {props.tag} </a></p>;
-};
+    var searchFlag = True;
+
     const MenuValues = [
     {
         id: 1,
@@ -50,14 +49,15 @@ export default function MainTrainer() {
         icon: <svg class="h-8 w-8 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />  <polyline points="14 2 14 8 20 8" />  <line x1="16" y1="13" x2="8" y2="13" />  <line x1="16" y1="17" x2="8" y2="17" />  <polyline points="10 9 9 9 8 9" /></svg>
     },
     ]
-
-    const subMenu = [
+    const headerMenu = [
+        
         {
             tittle: 'Usuarios',
             subTittle:'',
-            icon: <svg class="h-8 w-8 text-main-blue"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
-          </svg> 
+            icon: <svg class="h-10 w-10 text-main-blue"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/>
+                </svg>,
+            searchFlag
         }
     ]
 
@@ -70,7 +70,7 @@ export default function MainTrainer() {
         <hr className='bg-main-gold h-1'/>
         <div className={`m-0 h-screen bg-main-gray w-full`}>
             <div className='m-0'>
-            <SecondHeader options={subMenu}/>
+            <SecondHeader options={headerMenu}/>
             <List options={options}/>
             </div>
         </div>
