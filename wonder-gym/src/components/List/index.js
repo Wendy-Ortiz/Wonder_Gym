23154f0c151b286bbdce826e56d34821fa31d9db
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-06-29 13:11:38 -0600

    Fix code format

--- wonder-gym/src/components/List/index.js
+++ wonder-gym/src/components/List/index.js
@@ -6,9 +6,7 @@ export default function Lists(props){
         {props.options.map((input)=>{  
             return <div className="bg-main-gray md:text-lg lg:text-xl">
                     <Link classame="flex items-center" to={input.route}>
-                        <div className="flex px-8 md:px-12 lg:px-24 pb-8 pt-8 md:pb-10 
-                        md:pt-10 lg:pb-12 lg:pt-12 items-center font-bold text-white 
-                        hover:bg-main-blue-transparent border-b">
+                        <div className="flex px-8 md:px-12 lg:px-24 pb-8 pt-8 md:pb-10 md:pt-10 lg:pb-12 lg:pt-12 items-center font-bold text-white hover:bg-main-blue-transparent border-b">
                             <div className="h-10"></div>
                             <li className="flex items-center">
                                 <div className ="pr-8">
