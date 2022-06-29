f841a19420dc4c7b6f3a783781fe61989f1bc035
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-06-29 10:16:52 -0600

    component separation

--- wonder-gym/src/components/List/index.js
+++ wonder-gym/src/components/List/index.js
@@ -3,21 +3,14 @@ import { Link } from "react-router-dom";
 export default function Lists(props){
     return (
         <>
-   
-        {/*<div className="bg-white w-full sm:px-8 md:px-12 lg:px-24">
-            <br></br>
-            <h1 className="flex items-center w-10/12 pl-8 font-bold text-3xl" id="username"> ¡HOLA USERNAME! </h1>
-            <br></br>
-            <h2 className= "flex items-center w-10/12 pl-8 font-bold text-main-blue" id="trainerSubTittle">¿QUÉ DESEAS HACER HOY?</h2>
-            <br></br>
-        </div>*/}
-    {props.options.map((input)=>{  
-        
-            return <div className="w-full bg-main-gray">
+        {props.options.map((input)=>{  
+            return <div className="bg-main-gray md:text-lg lg:text-xl">
                     <Link classame="flex items-center" to={input.route}>
-                        <div className="flex items-center font-bold pl-8 md:pl-20 lg:pl-32 pb-8 pt-8 md:pb-10 md:pt-10 text-white hover:bg-main-blue-transparent border-b dark:border-white">
-                            <div className="flex items-center h-10"></div>
-                            <li className="flex items-center h-1 mr-4 ">
+                        <div className="flex px-8 md:px-12 lg:px-24 pb-8 pt-8 md:pb-10 
+                        md:pt-10 lg:pb-12 lg:pt-12 items-center font-bold text-white 
+                        hover:bg-main-blue-transparent border-b">
+                            <div className="h-10"></div>
+                            <li className="flex items-center">
                                 <div className ="pr-8">
                                     {input.icon}
                                 </div>
