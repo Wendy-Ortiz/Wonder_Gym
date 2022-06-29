ec7d7d11aa6814da28d2cc8d3e1ea8ab6a96123f
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-06-29 10:22:13 -0600

    added temporal data

--- wonder-gym/src/Pages/Trainer/TrainerUsers.js
+++ wonder-gym/src/Pages/Trainer/TrainerUsers.js
@@ -28,25 +28,24 @@ export default function MainTrainer() {
     {
         id: 1,
         tag: 'Alejandro Elvira Ramirez',
-        route: '/mainTrainer/trainerUsers'
+        route: '/mainTrainer/trainerUsers/alejandroElviraRamirez',
     },
     {
         id: 2,
         tag: 'Alejandro Elvira Ramirez',
-        route: '/mainTrainer/newRoutine'
+        route: '/mainTrainer/trainerUsers',
       
     },
     {
         id: 3,
-        tag: 'Entrenar',
-        route: '/mainTrainer/toTrain',
-        icon: <svg class="h-8 w-8 text-white"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="5" cy="18" r="3" />  <circle cx="19" cy="18" r="3" />  <polyline points="12 19 12 15 9 12 14 8 16 11 19 11" />  <circle cx="17" cy="5" r="1" /></svg>
+        tag: 'Alejandro Elvira Ramirez',
+        route: '/mainTrainer/trainerUsers',
+
     },
     {
         id: 4,
-        tag: 'Noticias',
-        route: '/mainTrainer/new',
-        icon: <svg class="h-8 w-8 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />  <polyline points="14 2 14 8 20 8" />  <line x1="16" y1="13" x2="8" y2="13" />  <line x1="16" y1="17" x2="8" y2="17" />  <polyline points="10 9 9 9 8 9" /></svg>
+        tag: 'Alejandro Elvira Ramirez',
+        route: '/mainTrainer/trainerUsers',
     },
     ]
     const headerMenu = [
