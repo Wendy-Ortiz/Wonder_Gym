351f7ccbdeaa7552abc65c82498aef562c88986b
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-06 00:37:07 -0600

    Add new validation for list

--- wonder-gym/src/components/List/index.js
+++ wonder-gym/src/components/List/index.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 export default function Lists(props){
     return (
         <>
-        {props.options.map((input)=>{  
+        {props && props.options.map((input)=>{  
             return <div className="bg-main-gray md:text-lg lg:text-xl">
                     <Link classame="flex items-center" to={input.route}>
                         <div className="flex px-8 md:px-12 lg:px-24 pb-8 pt-8 md:pb-10 md:pt-10 lg:pb-12 lg:pt-12 items-center font-bold text-white hover:bg-main-blue-transparent border-b">
                             <div className="h-10"></div>
                             <li className="flex items-center">
                                 <div className ="pr-8">
-                                    {input.icon}
+                                    {input.icon} 
                                 </div>
                                 <div>
                                     {input.tag}
