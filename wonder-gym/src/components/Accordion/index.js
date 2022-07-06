9b05d85611e5c623871f4bc35a1a9260049cb647
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-06 00:36:14 -0600

    extra changes in Accordion component

--- wonder-gym/src/components/Accordion/index.js
+++ wonder-gym/src/components/Accordion/index.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import {
   Accordion,
   AccordionHeader,
@@ -19,7 +19,7 @@ export default function AccordionList(props) {
             {/**TODO: Separe deploy for option*/}
             <Accordion open={open === 1} onClick={() => handleOpen(1)} className="bg-main-gray md:text-lg lg:text-xl">
                 <AccordionHeader className="flex px-8 md:px-12 lg:px-24 pb-8 pt-8 md:pb-10 md:pt-10 lg:pb-12 lg:pt-12 items-center font-bold text-white hover:bg-main-blue-transparent border-b">{input.tittle}</AccordionHeader>
-                <AccordionBody className="p-5 border border-t-0 bg-white border-gray-200 dark:border-gray-700">
+                <AccordionBody className="p-5 border border-t-0 bg-white border-white dark:border-gray-700">
                     <ul className="pl-5 list-disc text-main-gray dark:text-main-gray">
                         {/**TODO: Show all list*/}
                         {/**TODO: Responsive*/}
