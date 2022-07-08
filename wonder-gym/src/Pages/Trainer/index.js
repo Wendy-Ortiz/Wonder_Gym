9a0284d054c00faba1824020ca4352e94ea91201
AxelMatus <axelantonio@gmail.com>
2022-07-08 01:29:07 -0600

    Prototype icons now are used

--- wonder-gym/src/Pages/Trainer/index.js
+++ wonder-gym/src/Pages/Trainer/index.js
@@ -51,31 +51,31 @@ export default function MainTrainer() {
             id: 1,
             tag: 'Usuarios',
             route: '/mainTrainer/trainerUsers',
-            icon: <svg className="h-8 w-8 text-white"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"/></svg>
+            icon: <img src = "/icons/users_icon_white.svg" alt="Users Icon"/>,
         },
         {
             id: 2,
             tag: 'Nueva Rutina',
             route: '/mainTrainer/newRoutine',
-            icon: <svg className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/></svg>
+            icon: <img src = "/icons/routine_icon.svg" alt="New Routine Icon"/>,
         
         },
         {
             id: 3,
             tag: 'Entrenar',
             route: '/mainTrainer/toTrain',
-            icon: <svg className="h-8 w-8 text-white" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="5" cy="18" r="3" />  <circle cx="19" cy="18" r="3" />  <polyline points="12 19 12 15 9 12 14 8 16 11 19 11" />  <circle cx="17" cy="5" r="1" /></svg>
+            icon: <img src = "/icons/weightlifting_icon.svg" alt="To Train Icon"/>,
         },
         {
             id: 4,
             tag: 'Noticias',
             route: '/mainTrainer/news',
-            icon: <svg className="h-8 w-8 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor" strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />  <polyline points="14 2 14 8 20 8" />  <line x1="16" y1="13" x2="8" y2="13" />  <line x1="16" y1="17" x2="8" y2="17" />  <polyline points="10 9 9 9 8 9" /></svg>
+            icon: <img src = "/icons/news_icon.svg" alt="News Icon"/>,
         },
     ]
 
     return (
-        <>
+        <div className="bg-main-gray w-full h-screen" >
             <div className='m-0'>
             <Header bg_gray={true} logo_src='/logo84-64.png' values={MenuValues}/>
             </div>
@@ -87,11 +87,11 @@ export default function MainTrainer() {
                 <h2 className= "flex items-center px-8 md:px-12 lg:px-24 text-secundary-blue text-xl md:text-2xl lg:text-3xl" id="trainerSubTittle">¿QUÉ DESEAS REVISAR HOY?</h2>
                 <br></br>
             </div>
-            <div className={`m-0 h-screen bg-main-gray w-full`}>
+            <div className={`m-0`}>
                 <div className='m-0'>
                 <List options={options}/>
                 </div>
             </div>
-        </>
+        </div>
     );
   }
--- wonder-gym/src/Pages/User/index.js
+++ wonder-gym/src/Pages/User/index.js
@@ -49,31 +49,30 @@ export default function MainTrainer() {
             id: 1,
             tag: 'Mis rutinas',
             route: '/users/routine',
-            icon: <svg className="h-8 w-8 text-white"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M6 4h11a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-11a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1m3 0v18" />  <line x1="13" y1="8" x2="15" y2="8" />  <line x1="13" y1="12" x2="15" y2="12" /></svg>
+            icon: <img src = "/icons/routine2_icon.svg" alt="My Routines Icon"/>,
         },
         {
             id: 2,
             tag: 'Rutinas básicas',
             route: '/users/basicRoutine',
-            icon: <svg className="h-8 w-8 text-white"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"/></svg>
+            icon: <img src = "/icons/routine_icon.svg" alt="Basic Routines Icon"/>,
         },
         {
             id: 3,
             tag: 'Calentamientos',
             route: '/users/warmUps',
-            icon: <svg className="h-8 w-8 text-white"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="5" cy="18" r="3" />  <circle cx="19" cy="18" r="3" />  <polyline points="12 19 12 15 9 12 14 8 16 11 19 11" />  <circle cx="17" cy="5" r="1" /></svg>
+            icon: <img src = "/icons/warmUp_icon.svg" alt="Warm Up Icon"/>,
         },
         {
             id: 4,
             tag: 'Ver noticias',
             route: 'users/news',
-            icon: <svg className="h-8 w-8 text-white"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round">  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />  <polyline points="14 2 14 8 20 8" />  <line x1="16" y1="13" x2="8" y2="13" />  <line x1="16" y1="17" x2="8" y2="17" />  <polyline points="10 9 9 9 8 9" /></svg>
+            icon: <img src = "/icons/news_icon.svg" alt="News Icon"/>,
         },
     ]
 
     return (
-    <>
-        
+    <div className="bg-main-gray w-full h-screen">   
         <div className='m-0'>
           <Header bg_gray={true} logo_src='/logo84-64.png' values={MenuValues}/>
         </div>
@@ -85,11 +84,11 @@ export default function MainTrainer() {
                 <h2 className="flex items-center px-8 md:px-12 lg:px-24 text-secundary-blue text-xl md:text-2xl lg:text-3xl" id="userSubTittle">¿QUÉ DESEAS HACER HOY?</h2>
                 <br></br>
         </div>
-        <div className={`m-0 h-screen bg-main-gray w-full`}>
+        <div className={`m-0 bg-main-gray w-full`}>
             <div className='m-0'>
             <List options={options}/>
             </div>
         </div>
-    </>
+    </div>
     );
   }
