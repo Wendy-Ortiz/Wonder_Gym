637812d35f0dd0cc4a136f93e8cbc2658c6f8662
AxelMatus <axelantonio@gmail.com>
2022-07-08 02:44:56 -0600

    Fixed som errors

--- wonder-gym/src/components/List/index.js
+++ wonder-gym/src/components/List/index.js
@@ -7,7 +7,7 @@ export default function Lists(props){
         {props && props.options.map((input)=>{
         return (
         <div key={input.id} className="bg-main-gray md:text-lg lg:text-xl">
-            <Link className="items-center" to={input.route}>
+            <Link className="items-center" to={input.route ? input.route : `/mainTrainer/trainerUsers/${input.name}` }>
                 <div className="flex px-8 md:px-12 lg:px-24 pb-8 pt-8 md:pb-10 md:pt-10 lg:pb-12 lg:pt-12 items-center font-bold text-white hover:bg-main-blue-transparent border-b">
                     <div className="h-10"></div>
                     <div className="flex items-center">
@@ -15,7 +15,7 @@ export default function Lists(props){
                             {input.icon} 
                         </div>
                         <div className="font-montserrat">
-                            {input.tag}
+                            {input.tag ? input.tag : input.name}
                         </div>
                     </div>
                 </div>
