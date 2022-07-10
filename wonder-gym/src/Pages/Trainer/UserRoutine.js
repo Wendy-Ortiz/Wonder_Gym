4d3c23e4599fd15fc6b5aba2a9a1609e21b3cc92
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-10 10:44:38 -0600

    Fetch for userRoutine

--- wonder-gym/src/Pages/Trainer/UserRoutine.js
+++ wonder-gym/src/Pages/Trainer/UserRoutine.js
@@ -1,7 +1,6 @@
-import React from 'react'; 
-import jwtDecode from "jwt-decode";  
-import { useDispatch } from "react-redux";
-import { useNavigate  } from 'react-router-dom'  
+import {React, useState, useEffect } from 'react'; 
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate  } from 'react-router-dom' 
 import Header from '../../components/Header'
 import List from '../../components/List'
 import SecondHeader from '../../components/SecondHeader'
@@ -12,20 +11,31 @@ export default function SelectExercises() {
     var searchFlag = false;
     const dispatch = useDispatch();
     const navigate = useNavigate ();
-    const token = localStorage.getItem('token');
-    const decryptedToken = jwtDecode(token);
-    const name = (decryptedToken.userName).split(" ");
+    const name = useSelector(
+        (state) => state.user.name
+    );
+    const id = useSelector(
+        (state) => state.user.id
+    );
 
-    /*const [options,setOptions] = useState(null);
+    const [options,setOptions] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(()=> {
         const fetchUsers = async () => {
+
             const token = localStorage.getItem('token');
-            try{
+
+            try {
                 const usersFetch = await fetch('http://localhost:3001/users/user', {
-                    method: "GET",
-                    headers: { "authorization": `Bearer ${token}` }
+                    method: 'POST',
+                    headers: {
+                        "Content-type": "application/json",
+                        "authorization": `Bearer ${token}`
+                    },
+                    body: JSON.stringify({
+                    id: {id}
+                    }),
                 });
                 const usersJSON = await usersFetch.json();
                 console.log('Lo logro ', usersJSON);
@@ -35,10 +45,11 @@ export default function SelectExercises() {
                 console.log('Ay fallo :c')
                 console.error(error);
             }
+
         }
         fetchUsers();
-
-    }, []);*/
+            
+        });
 
     const MenuValues = [
         {
@@ -58,50 +69,10 @@ export default function SelectExercises() {
         },
     ]
 
-    const options = [
-        {
-            id: 1,
-            tag: 'Rutina Lunes',
-            route: `/mainTrainer/${name[0]}/rutinaLunes`,
-            icon: "",
-        },
-        {
-            id: 2,
-            tag: 'Rutina Martes',
-            route: `/mainTrainer/${name[0]}/rutinaLunes`,
-            icon: "",
-        },
-        {
-            id: 1,
-            tag: 'Rutina Lunes',
-            route: `/mainTrainer/${name[0]}/rutinaLunes`,
-            icon: "",
-        },
-        {
-            id: 2,
-            tag: 'Rutina Martes',
-            route: `/mainTrainer/${name[0]}/rutinaLunes`,
-            icon: "",
-        },
-        {
-            id: 1,
-            tag: 'Rutina Lunes',
-            route: `/mainTrainer/${name[0]}/rutinaLunes`,
-            icon: "",
-        },
-        {
-            id: 2,
-            tag: 'Rutina Martes',
-            route: `/mainTrainer/${name[0]}/rutinaLunes`,
-            icon: "",
-        },
-
-
-    ]
     const headerMenu = [
         
         {
-            title:  name[0],
+            title:  name,
             subTittle:'',
             searchFlag
         }
@@ -109,46 +80,50 @@ export default function SelectExercises() {
 
    
     return (
-    <>
-    <div>
-        <div className='m-0'>
-          <Header bg_gray={true} logo_src='/logo84-64.png' values={MenuValues}/>
-        </div>
-        <hr className='bg-main-gold h-1'/>
-        <div className={`m-0 h-screen bg-main-gray`}>
-            <div className='m-0'>
-                <SecondHeader options={headerMenu}/>
-            </div>
-            <div key={options.id} className="items-center bg-main-gray w-full font-montserrat">
-                <p className="items-center text-white font-bold text-lg pl-16 pt-4" > Datos:</p>
-                <div className="flex pl-16 pt-4 columns-2">
-                    <div className="flex items-center">
-                        <img src = "/icons/weight_Kg.svg" alt="News Icon"/>
-                        <p className="items-center text-white font-extralight text-base px-4"> {/*contraindications[0]*/}80.56 kg</p>
-
-                    </div>
-                    <div className="flex items-center p-2">
-                        <img src = "/icons/height.svg" alt="News Icon"/>,
-                        <p className="items-center text-white font-extralight text-base px-2"> {/*contraindications[0]*/} 1.56kg</p>
-                    </div>
-                </div>
-                <p className="items-center text-white font-bold text-lg pl-16 pt-4">Contraindicaciones:</p>
+        loading ? <div className='fixed top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>
+            <div className='w-16 h-16 border-b-2 border-main-blue rounded-full animate-spin'></div>
+        </div> : (
+            <>
                 <div>
-                    <p className="items-center text-white font-extralight text-base pl-16"> {/*contraindications[0]*/} No tiene contraaindicaciones </p>
+                    <div className='m-0'>
+                    <Header bg_gray={true} logo_src='/logo84-64.png' values={MenuValues}/>
+                    </div>
+                    <hr className='bg-main-gold h-1'/>
+                    <div className={`m-0 h-screen bg-main-gray`}>
+                        <div className='m-0'>
+                            <SecondHeader options={headerMenu}/>
+                        </div>
+                        <div key={options.id} className="items-center bg-main-gray w-full font-montserrat">
+                            <p className="items-center text-white font-bold text-lg pl-16 pt-4" > Datos:</p>
+                            <div className="flex pl-16 pt-4 columns-2">
+                                <div className="flex items-center">
+                                    <img src = "/icons/weight_Kg.svg" alt="News Icon"/>
+                                    <p className="items-center text-white font-extralight text-base px-4"> {/*contraindications[0]*/}80.56 kg</p>
+
+                                </div>
+                                <div className="flex items-center p-2">
+                                    <img src = "/icons/height.svg" alt="News Icon"/>,
+                                    <p className="items-center text-white font-extralight text-base px-2"> {/*contraindications[0]*/} 1.56kg</p>
+                                </div>
+                            </div>
+                            <p className="items-center text-white font-bold text-lg pl-16 pt-4">Contraindicaciones:</p>
+                            <div>
+                                <p className="items-center text-white font-extralight text-base pl-16"> {/*contraindications[0]*/} No tiene contraaindicaciones </p>
+                            </div>
+                            <br></br>
+                        </div>
+                        <FixButton/>
+                        <div>
+                            <div className="flex items-center px-8 md:px-12 lg:px-24 pb-8 pt-8 md:pb-10 md:pt-10 lg:pb-12 lg:pt-12 bg-white font-bold text-secundary-blue border-b">
+                                <h1 className="flex items-center px-8 md:px-12 lg:px-24  text-3xl md:text-4x1 lg:text-5xl" id="username">RUTINAS</h1>
+                                <img  className="flex items-center" src = "/icons/routine2_icon_blue.svg" alt="New Routine Icon"/>
+                            </div>
+                            <List options={options}/> {/*<AccordionList options={options}/> */}
+                        </div>
+                    </div>
                 </div>
-                <br></br>
-            </div>
-            <FixButton/>
-            <div>
-                <div className="flex items-center px-8 md:px-12 lg:px-24 pb-8 pt-8 md:pb-10 md:pt-10 lg:pb-12 lg:pt-12 bg-white font-bold text-secundary-blue border-b">
-                    <h1 className="flex items-center px-8 md:px-12 lg:px-24  text-3xl md:text-4x1 lg:text-5xl" id="username">RUTINAS</h1>
-                    <img  className="flex items-center" src = "/icons/routine2_icon_blue.svg" alt="New Routine Icon"/>
-                </div>
-                <List options={options}/> {/*<AccordionList options={options}/> */}
-            </div>
-        </div>
-    </div>
-    </>
+            </>
+        )
     
     );
   }
--- wonder-gym/src/components/FixButton/index.js
+++ wonder-gym/src/components/FixButton/index.js
@@ -1,7 +1,7 @@
 export default function FixButton() {
   return (    
     <div className="relative">
-        <button className="fixed bottom-0 right-0 rounded hover:bg-secundary-blue">
+        <button className="fixed bottom-0 right-0 rounded hover:bg-secundary-blue p-4 m-4">
             <img src = "/icons/plus.svg" alt="News Icon"/>
         </button>
     </div>
