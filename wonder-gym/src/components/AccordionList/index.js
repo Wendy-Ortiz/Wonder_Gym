d7960064a6bb8940b04437df37c143db96e1cd97
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-07-10 22:01:48 -0600

    add flag

--- wonder-gym/src/components/AccordionList/index.js
+++ wonder-gym/src/components/AccordionList/index.js
@@ -5,11 +5,10 @@ export default function AccordionList(props){
     return (
         <>
         {props && (props.options).map((input)=>{
-            console.log(input)
             return <div key={input.id} className="bg-main-gray md:text-lg lg:text-xl">
-                <li className="flex items-center">
-                    <AccordionComponent className="flex items-center" options={input.exercises}/>
-                </li>
+                <div className="flex items-center">
+                    <AccordionComponent className="flex items-center" options={input.exercises} checkboxFlag={props.checkboxFlag}/>
+                </div>
             </div>
             })}
         </>
